97ac011be0be20540287658125ed659fe99842bc
Minhaj Jami <jami.minhaj@gmail.com>
2019-08-01 22:38:16 +0600

    add Dynamic example

---
+++ Dynamic.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+{
+    public static void Main()
+    {
+        dynamic something = 'x';
+        something = 123;
+        something = 10.3f;
+        something = "Minhaj Jami";
+
+        Console.WriteLine(something);
+        
+    }
+}
+
